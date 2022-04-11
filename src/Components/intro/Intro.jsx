99f691c10fdfc97f2267292c976c7640c14a7a56
Syifa Ainnur <arwahitem01@gmail.com>
2022-04-11 14:04:41 +0700

    update floating icon

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -9,6 +9,7 @@ import boy from "../../img/boy.png";
 import thumbup from "../../img/thumbup.png";
 import Crown from "../../img/crown.png";
 import glassesimoji from "../../img/glassesimoji.png";
+import FloatingDiv from "../FloatingDiv/FloatingDiv";
 function Intro() {
     return (
         <div className="intro">
@@ -38,6 +39,19 @@ function Intro() {
                 <img src={Vector1} alt="Vector1" />
                 <img src={Vector2} alt="Vector2" />
                 <img src={boy} alt="boy" />
+                <img src={glassesimoji} alt="" />
+                <div style={{top: '-4%', left: '58%' }}>
+                    <FloatingDiv image={Crown} txt1='Web' txt2='Developer' />
+                </div>
+                <div style={{top: '18rem', left: '0rem' }}>
+                    <FloatingDiv image={thumbup} txt1='Best Design' txt2='Award' />
+                </div>
+
+                {/* Blur div */}
+
+                <div className="blur" style={{background: 'rgb(238 210 255)'}}>
+                <div className="blur" style={{background: '#C1F5FF', top: '17rem', width: '21rem', height: '11rem', left: '-9rem'}}></div>
+                </div>
             </div>
         </div>
     );
