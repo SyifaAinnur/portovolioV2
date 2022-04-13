8cb89802851e08c05864c06a444d49bbe55fda00
Syifa Ainnur <arwahitem01@gmail.com>
2022-04-13 14:33:46 +0700

    Update Intro.jsx

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -13,81 +13,81 @@ import FloatingDiv from "../FloatingDiv/FloatingDiv";
 import { motion } from "framer-motion";
 import { Link } from "react-scroll";
 function Intro() {
-  const transition = { duration: 2, type: "spring" };
+    const transition = { duration: 2, type: "spring" };
 
-  return (
-    <div className="intro">
-      <div className="i-left">
-        <div className="i-name">
-          <span>Hy! I Am</span>
-          <span>Syifa Ain'nur</span>
-          <span>
-            Frontend Developer with high level of experience in web designing
-            and development, <br></br>producting the Quality work
-          </span>
+    return (
+        <div className="intro">
+            <div className="i-left">
+                <div className="i-name">
+                    <span>Hy! I Am</span>
+                    <span>Syifa Ain'nur</span>
+                    <span>
+                        Frontend Developer with high level of experience in web designing
+                        and development, <br></br>producting the Quality work
+                    </span>
+                </div>
+                <button className="button i-button">Hire Me!!</button>
+                <div className="i-icons">
+                    <a href="#">
+                        <img src={Github} alt="Github" />
+                    </a>
+                    <a href="#">
+                        <img src={LinkedIn} alt="LinkedIn" />
+                    </a>
+                    <a href="#">
+                        <img src={Instagram} alt="Instagram" />
+                    </a>
+                </div>
+            </div>
+            <div className="i-right">
+                <img src={Vector1} alt="Vector1" />
+                <img src={Vector2} alt="Vector2" />
+                <img src={boy} alt="boy" />
+                {/* animation */}
+                <motion.img
+                    initial={{ left: "-36%" }}
+                    whileInView={{ left: "-24%" }}
+                    transition={transition}
+                    src={glassesimoji}
+                    alt=""
+                />
+
+                <motion.div
+                    initial={{ top: "-4%", left: "74%" }}
+                    whileInView={{ left: "68%" }}
+                    transition={transition}
+                    className="floating-div"
+                >
+                    <FloatingDiv image={Crown} txt1="Web" txt2="Developer" />
+                </motion.div>
+                {/* animation */}
+                <motion.div
+                    initial={{ left: "9rem", top: "18rem" }}
+                    whileInView={{ left: "0rem" }}
+                    transition={transition}
+                    className="floating-div"
+                >
+                    {/* floatinDiv mein change hy dark mode ka */}
+                    <FloatingDiv image={thumbup} txt1="Best Design" txt2="Award" />
+                </motion.div>
+
+                {/* Blur div */}
+
+                <div className="blur" style={{ background: "rgb(238 210 255)" }}>
+                    <div
+                        className="blur"
+                        style={{
+                            background: "#C1F5FF",
+                            top: "17rem",
+                            width: "21rem",
+                            height: "11rem",
+                            left: "-9rem",
+                        }}
+                    ></div>
+                </div>
+            </div>
         </div>
-        <button className="button i-button">Hire Me!!</button>
-        <div className="i-icons">
-          <a href="#">
-            <img src={Github} alt="Github" />
-          </a>
-          <a href="#">
-            <img src={LinkedIn} alt="LinkedIn" />
-          </a>
-          <a href="#">
-            <img src={Instagram} alt="Instagram" />
-          </a>
-        </div>
-      </div>
-      <div className="i-right">
-        <img src={Vector1} alt="Vector1" />
-        <img src={Vector2} alt="Vector2" />
-        <img src={boy} alt="boy" />
-        {/* animation */}
-        <motion.img
-          initial={{ left: "-36%" }}
-          whileInView={{ left: "-24%" }}
-          transition={transition}
-          src={glassesimoji}
-          alt=""
-        />
-
-        <motion.div
-          initial={{ top: "-4%", left: "74%" }}
-          whileInView={{ left: "68%" }}
-          transition={transition}
-          className="floating-div"
-        >
-          <FloatingDiv image={Crown} txt1="Web" txt2="Developer" />
-        </motion.div>
-        {/* animation */}
-        <motion.div
-          initial={{ left: "9rem", top: "18rem" }}
-          whileInView={{ left: "0rem" }}
-          transition={transition}
-          className="floating-div"
-        >
-          {/* floatinDiv mein change hy dark mode ka */}
-          <FloatingDiv image={thumbup} txt1="Best Design" txt2="Award" />
-        </motion.div>
-
-        {/* Blur div */}
-
-        <div className="blur" style={{ background: "rgb(238 210 255)" }}>
-          <div
-            className="blur"
-            style={{
-              background: "#C1F5FF",
-              top: "17rem",
-              width: "21rem",
-              height: "11rem",
-              left: "-9rem",
-            }}
-          ></div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Intro;
